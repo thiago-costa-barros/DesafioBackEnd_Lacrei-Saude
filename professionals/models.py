631b983cc29a4d7988203a0d6f0ff02b12424631
Seu Nome <seu@email.com>
2025-02-15 14:51:17 -0300

    feat: add profession model

--- professionals/models.py
+++ professionals/models.py
@@ -4,7 +4,7 @@ class HealthProfessional(models.Model):
     id = models.AutoField(primary_key=True, db_column="HealthProfessionalId")
     full_name = models.CharField(max_length=255, db_column="TrandingName")
     social_name = models.CharField(max_length=255, blank=True, null=True, db_column="SocialName")
-    profession = models.CharField(max_length=100, db_column="Profession")
+    profession = models.ForeignKey('Profession', on_delete=models.DO_NOTHING, db_column="ProfessionId")
     zipcode = models.CharField(max_length=10, db_column="ZipCode") 
     adress_street = models.TextField(max_length=255, db_column="AddressStreet")
     adress_neighborhood = models.TextField(max_length=255, db_column="AddressNeighborhood")
@@ -17,5 +17,23 @@ class HealthProfessional(models.Model):
     updated_at = models.DateTimeField(auto_now=True, db_column="UpdateDate")
     deleted_at = models.DateTimeField(null=True, blank=True, db_column="DeletionDate")
 
+    class Meta:
+        db_table = "HealthProfessional"
+    
     def __str__(self):
         return self.social_name if self.social_name else self.full_name
+    
+class Profession(models.Model):
+    id = models.AutoField(primary_key=True, db_column="ProfessionId")
+    name = models.CharField(max_length=100, db_column="Name")
+    description = models.TextField(max_length=255, db_column="Description")
+    
+    created_at = models.DateTimeField(auto_now_add=True, db_column="CreationDate")
+    updated_at = models.DateTimeField(auto_now=True, db_column="UpdateDate")
+    deleted_at = models.DateTimeField(null=True, blank=True, db_column="DeletionDate")
+    
+    class Meta:
+        db_table = "Profession"
+
+    def __str__(self):
+        return self.name
