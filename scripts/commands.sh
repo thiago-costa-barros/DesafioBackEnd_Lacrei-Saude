9f2dd0c8c8b8814f0f446b4b15280ae2d6065821
Seu Nome <seu@email.com>
2025-02-25 19:50:08 -0300

    feat: docker config completa

---
+++ scripts/commands.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# O shell irá encerrar a execução do script quando um comando falhar
+set -e
+
+while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+  echo "🟡 Aguardando o Postgres Database ($POSTGRES_HOST $POSTGRES_PORT) ..."
+  sleep 2
+done
+
+echo "✅ Postgres Database Iniciado com sucesso: ($POSTGRES_HOST:$POSTGRES_PORT)"
+
+python manage.py collectstatic --noinput
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+python manage.py runserver 0.0.0.0:8000
